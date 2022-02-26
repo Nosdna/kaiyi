d76c3e533da93645461f39ca617719ad7cc737f4
Nosdna <88096569+Nosdna@users.noreply.github.com>
2022-02-26 15:34:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ def index():
         return(render_template("index.html", default = s))
     else:
         return(render_template("index.html", default = "Predict Default"))
-app.run()
+if __name__ == "__main__":
+    app.run()
 
 
 # In[ ]:
