d375dec66696f9567fdacdbb4d061ebe1fea6191
Nosdna <88096569+Nosdna@users.noreply.github.com>
2022-02-26 15:12:53 +0800

    Add files via upload

---
+++ App.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask
+app = Flask(__name__)
+
+import joblib
+from flask import request, render_template
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        income = request.form.get("income")
+        age = request.form.get("age")
+        loan = request.form.get("loan")
+        print(income, age, loan)
+        model=joblib.load("CreditDefault")
+    #normalising the inputs
+        pred=model.predict([[((float(income)-45136.87597485758)/14423.382398409489),
+                             ((float(age)-34.795949606582)/12.838181998141051),
+                             ((float(loan)-5591.986694851936)/3174.059368326566)]])
+        print(pred)
+        s="The predicted default is: " + str(pred[0])
+        return(render_template("index.html", default = s))
+    else:
+        return(render_template("index.html", default = "Predict Default"))
+app.run()
+
+
+# In[ ]:
+
+
+
+
